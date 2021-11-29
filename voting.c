5176ed2359355d50a33116ce3b56e1145e9ef845
Meghana2004 <93900990+Meghana2004@users.noreply.github.com>
2021-11-28 19:18:35 -1200

    Create voting.c

---
+++ voting.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int age;
+printf("enter your age");
+scanf("%d",&age);
+if(age>=18)
+printf("your'e eligible for voting");
+else
+printf("your'e not eligible for voting");
+}
